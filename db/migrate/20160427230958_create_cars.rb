5413c627622f3640f74e9e301cb13f7638c3bb4d
Jason Swett <jason@benfranklinlabs.com>
2016-04-27 19:12:09 -0400

    Improve cars table.

--- db/migrate/20160427230958_create_cars.rb
+++ db/migrate/20160427230958_create_cars.rb
@@ -1,11 +1,13 @@
 class CreateCars < ActiveRecord::Migration
   def change
     create_table :cars do |t|
-      t.string :make
-      t.string :model
-      t.string :year
+      t.string :make, null: false
+      t.string :model, null: false
+      t.string :year, null: false
 
       t.timestamps null: false
     end
+
+    add_index :cars, %i(make model year), unique: true
   end
 end
