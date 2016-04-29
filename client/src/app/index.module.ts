463941a07d6a338af7225e68a6e89721d11d375e
Jason Swett <jason@benfranklinlabs.com>
2016-04-29 15:56:47 -0400

    Remove cruft.

--- client/src/app/index.module.ts
+++ client/src/app/index.module.ts
@@ -3,13 +3,8 @@
 import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
-import { MainController } from './main/main.controller';
 import { CarsController } from './components/cars.controller';
 import { CarsNewController } from './components/cars-new.controller';
-import { GithubContributor } from '../app/components/githubContributor/githubContributor.service';
-import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
-import { acmeNavbar } from '../app/components/navbar/navbar.directive';
-import { acmeMalarkey } from '../app/components/malarkey/malarkey.directive';
 
 declare var malarkey: any;
 declare var moment: moment.MomentStatic;
@@ -29,18 +24,11 @@ module dreamCars {
     'ui.bootstrap',
     'toastr',
     'rails'
-  ]).constant('malarkey', malarkey)
-    .constant('moment', moment)
-    .config(config)
+  ]).config(config)
     .config(routerConfig)
     .run(runBlock)
-    .service('githubContributor', GithubContributor)
-    .service('webDevTec', WebDevTecService)
-    .controller('MainController', MainController)
     .controller('CarsController', CarsController)
     .controller('CarsNewController', CarsNewController)
-    .directive('acmeNavbar', acmeNavbar)
-    .directive('acmeMalarkey', acmeMalarkey)
     .factory('Car', (railsResourceFactory: any) => {
       return railsResourceFactory({
         url: '/api/cars',
