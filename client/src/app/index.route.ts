5070bfb5ff084632e8a1f205f64ebf1df6940874
Jason Swett <jason@benfranklinlabs.com>
2016-04-29 12:24:27 -0400

    Remove Home.

--- client/src/app/index.route.ts
+++ client/src/app/index.route.ts
@@ -2,11 +2,6 @@
 export function routerConfig($routeProvider: angular.route.IRouteProvider) {
   $routeProvider
     .when('/', {
-      templateUrl: 'app/main/main.html',
-      controller: 'MainController',
-      controllerAs: 'main'
-    })
-    .when('/cars', {
       templateUrl: 'app/components/cars.html',
       controller: 'CarsController',
       controllerAs: 'cars'
