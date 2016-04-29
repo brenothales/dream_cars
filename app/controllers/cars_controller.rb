436085aaf681b8bc9ce89f69ff6c3798d1245b13
Jason Swett <jason@benfranklinlabs.com>
2016-04-29 15:04:24 -0400

    Get car creation working.

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -4,4 +4,20 @@ class CarsController < ApplicationController
 
     render json: @cars
   end
+
+  def create
+    @car = Car.new(car_params)
+
+    if @car.save
+      render json: @car, status: :created, location: @car
+    else
+      render json: @car.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    
+  def car_params
+    params.require(:car).permit(:year, :make, :model)
+  end
 end
--- client/src/app/components/cars-new.controller.ts
+++ client/src/app/components/cars-new.controller.ts
@@ -1,5 +1,15 @@
 export class CarsNewController {
-  constructor(Car: any) {
+  public car;
+  public $location;
+
+  constructor(Car: any, $location: any) {
     /* tslint:disable */ 'ngInject'; /* tslint:enable */
+
+    this.car = new Car();
+    this.$location = $location;
+  }
+
+  save(car: any) {
+    car.save().then(() => this.$location.path('/'));
   }
 }
